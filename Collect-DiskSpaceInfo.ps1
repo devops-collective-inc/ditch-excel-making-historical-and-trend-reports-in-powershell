f70ce8867371c41abebfdca8f13575c430c918a4
The DevOps Collective <donj@powershell.org>
2016-05-13 08:29:35 -0700

    Add files

---
+++ Collect-DiskSpaceInfo.ps1
@@ -0,0 +1,33 @@
+﻿[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$True,ValueFromPipeline=$True)]
+    [string[]]$ComputerName
+)
+PROCESS {
+    foreach ($computer in $computername) {
+        try {
+            $params = @{'ComputerName'=$computer;
+                        'Filter'="DriveType=3";
+                        'Class'='Win32_LogicalDisk';
+                        'ErrorAction'='Stop'}
+            $ok = $True
+            $disks = Get-WmiObject @params
+        } catch {
+            Write-Warning "Error connecting to $computer"
+            $ok = $False
+        }
+
+        if ($ok) {
+            foreach ($disk in $disks) {
+                $properties = @{'ComputerName'=$computer;
+                                'DeviceID'=$disk.deviceid;
+                                'FreeSpace'=$disk.freespace;
+                                'Size'=$disk.size;
+                                'Collected'=(Get-Date)}
+                $obj = New-Object -TypeName PSObject -Property $properties
+                $obj.PSObject.TypeNames.Insert(0,'Report.DiskSpaceInfo')
+                Write-Output $obj
+            }
+        }                       
+    }
+}
---
+++ PerformanceCheck.ps1
@@ -0,0 +1,11 @@
+﻿$Datagrams = Get-Counter -Counter "\IPv4\Datagrams/sec" -SampleInterval 5 -MaxSamples 3
+$DataGrams | 
+ForEach-Object { 
+    $props = @{'ComputerName'=(Get-Content Env:\COMPUTERNAME);
+               'IPv4Datagrams'=($_.CounterSamples.CookedValue);
+               'Collected'=($_.CounterSamples.TimeStamp)}
+    $obj = New-Object -TypeName PSObject -Property $props
+    $obj.PSObject.TypeNames.Insert(0,'Report.IPv4DatagramsPerSec')
+    Write-Output $obj
+} |
+Save-ReportData -ConnectionString "Server=myServerAddress;Database=myDataBase;Trusted_Connection=True;"
